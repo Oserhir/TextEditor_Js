00885ae81118422ffda8630d606f407ca796143d
OSERHIRDEV <oserhir10@gmail.com>
2022-06-04 14:15:16 +0100

    Add a .italic classes to 'Formatted Text'

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 let text_input = document.getElementById("text-input");
 let text_output = document.getElementById("text-output");
 
+// Update the text in the "Formatted Text" section
 text_input.addEventListener("input", () => {
   text_output.innerText = text_input.value;
 });
 
+// Add a .bold classes to 'Formatted Text'
 let bold = document.getElementById("bold");
 
 bold.addEventListener("click", () => {
   text_output.classList.toggle("bold");
   bold.classList.toggle("active");
 });
+
+// Add a .italic classes to 'Formatted Text'
+let italic = document.getElementById("italic");
+
+italic.addEventListener("click", () => {
+  text_output.classList.toggle("italic");
+  italic.classList.toggle("active");
+});
+
+
