41e0e7fbb6cc781597ae58ff51f14c4e54b8939c
OSERHIRDEV <oserhir10@gmail.com>
2022-06-04 14:12:40 +0100

    Add a .bold classes to 'Formatted Text'

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ let text_output = document.getElementById("text-output");
 text_input.addEventListener("input", () => {
   text_output.innerText = text_input.value;
 });
+
+let bold = document.getElementById("bold");
+
+bold.addEventListener("click", () => {
+  text_output.classList.toggle("bold");
+  bold.classList.toggle("active");
+});
